54e9366782b9df657bf49ea44a2bee93f28509fe
Andromeda127645 <royalizyt@gmail.com>
2024-10-20 21:10:51 -0600

    bug fix

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ clients = []
 players = {}
 moves = {}
 
-def broadcast(message, sender_socket):
+def broadcast(message, sender_socket=None):
     for client in clients:
         if client != sender_socket:
             try:
@@ -36,28 +36,33 @@ def handle_client(client_socket, address):
             message = json.loads(message)
             if message["type"] == "join":
                 player_name = message['player_name']
-                players[player_name] = None
+                players[player_name] = client_socket
+                moves[player_name] = None
                 print(f"{message['player_name']} has joined the game.")
                 broadcast(f"Player {player_name} has joined the game.", client_socket)
                 
                 if len(players) == 2:
-                    broadcast("Both players have joined. The game can now start!")
+                    broadcast("Both players have joined. The game can now start!",None)
                 else:
-                    client_socket.send(json.dumps({"type": "info", "message": "Waiting for Player 2 to join..."}).encode("utf-8"))
+                    broadcast("Waiting for Player 2 to join...", None)
+                    
             elif message["type"] == 'move':
                 move = message['move']
-                players[player_name] = move  
+                moves[player_name] = move  
                 print(f"Received move from {address}: {message['move']}")
                 
                 if len(moves) == 2 and all(moves.values()):
                     player1, player2 = list(moves.keys())
                     move1, move2 = moves[player1], moves[player2]
-                    broadcast(f"{player_name} chose {move}.", client_socket)
-                    moves[player1], moves[player2] = None, None
 
+                    broadcast(f"{player1} chose {move1}. {player2} chose {move2}.")
+
+                    moves[player1], moves[player2] = None, None
+                    
             elif message["type"] == 'chat':
                 chat_message = f"Player {player_name}: {message['message']}"
                 broadcast(chat_message, client_socket)
+                
             elif message["type"] == 'quit':
                 players.pop(player_name, None)
                 print(f"Player {player_name} has quit.")
